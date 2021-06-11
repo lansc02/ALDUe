f8eb79251727c0f9ab14717bebfd27fc138751aa
stephan <stephan.lanegger@edu.campus02.at>
2021-06-11 12:14:33 +0200

    A12_Zustandsautomat - CSV Parser: Tests bis einschließlich Threequotedvalues funktionieren, Rest noch nicht.

--- ALD-Aufgabenstellungen-Starter/src/A12_Zustandsautomat/CSVParser_1.java
+++ ALD-Aufgabenstellungen-Starter/src/A12_Zustandsautomat/CSVParser_1.java
@@ -11,16 +11,68 @@ public class CSVParser_1 {
 	public static CSVResult parse(String str) {
 		
 		int state = 0;
-		
+		CSVResult res = new CSVResult();
 		for (int i = 0; i < str.length(); i++) {
 			char c = str.charAt(i);
 			
 			switch(state) {
+				case 0: //Start
+					if (c == '"'){
+						state = 2;
+					} else if (isTextData(c)){
+						res.appendChar(c);
+						state = 2;
+					}
+
+					else{
+						res = CSVResult.ERROR;
+						state=99;
+					}
+					break;
+				case 1: //Anführungszeichen
+					if (isTextData(c)){
+						res.appendChar(c);
+					}else if (c=='"'){
+					state=2;
+				}
+					break;
+				case 2: //Text
+                    if (isTextData(c)){
+                    	res.appendChar(c);
+					} else if(!isTextData(c) && c == ','){
+                    	state=3;
+                    	res.addValue();
+					}else if(c=='\r'){
+						state =4;
+					}
+					break;
+				case 3: //Beistrich
+					if (isTextData(c)){
+						res.appendChar(c);
+						state = 2;
+					} else if(c=='"'){
+						state=1;
+					} else {
+						state=99;
+						res=CSVResult.ERROR;
+					}break;
+				case 4: //Line end
+					if (c=='\n'){
+						//end of line
+
+					}
+					break;
+				case 99: //Error
+					return res;
+				default :
+					System.out.println("Undefined state, breaking.");
+					break;
 
 			} // switch end
 		}
-		
-		return null;
+
+		res.addValue();
+		return res;
 	}
 	
 	private static boolean isTextData(char c) {
